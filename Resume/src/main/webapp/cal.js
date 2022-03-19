ec69cb4ae1a3d813f14e311f2fb061c621afbf05
Ahamedal <94349140+Ahamedal@users.noreply.github.com>
2022-03-19 20:44:52 +0530

    Add files via upload

--- Resume/src/main/webapp/cal.js
+++ Resume/src/main/webapp/cal.js
@@ -2,41 +2,50 @@
  * 
  */
  var store=0;
- var store2=0;
+ var temp=0;
  var x=0;
  var a='';
  var k='';
  var s=0;
  var flag=true;	
+ var flag2=true;
+ var temp2='';
  function number(no){
+	if(temp!=0||temp2!=''){
+		document.getElementById("display").value="";
+	}
 	
 	document.getElementById("display").value+=no;
+	flag2=true;
+	temp2='';
+	flag=true;
    
      
  }
 function result(r){
 	var y=0;
 	flag=true;
-	if(r=='c'){
-		document.getElementById("display").value="";
-		store=0;
-         s=0;
-         a='';
-         k='';
-    document.getElementById("plus").style.background="";
-	document.getElementById("minus").style.background="";
-	document.getElementById("mul").style.background="";
-	document.getElementById("div").style.background="";
-	}
-	
-	else{
 	document.getElementById("plus").style.background="";
 	document.getElementById("minus").style.background="";
 	document.getElementById("mul").style.background="";
 	document.getElementById("div").style.background="";
+	if(r=='c'){
+		document.getElementById("display").value="";
+		document.getElementById("dis").value="";
+		store=0;
+         s=0;
+         a='';
+         k='';
+          temp2='';
+           temp=0;
+    
+	}
+	
+	else{
+	
    x=Number(document.getElementById("display").value);
-  
-   if(a=='+'){
+ 
+  if(a=='+'){
   y=store+x;
   }
   else if(a=='-'){
@@ -50,9 +59,16 @@ function result(r){
   else if(a=='/'){
   y=store/x;
   }
+  else{
+	y=x;
+}
   document.getElementById("display").value=y;
+  temp=y;
+ temp2='';
   store=0;
+  s=0;
   a='';
+  k='';
   }
 
 }
@@ -61,86 +77,113 @@ function operator(no){
 	
 	if(store==0){
 	store=Number(document.getElementById("display").value);
-	}else{
+	}
+	else if(flag2){
+		 
     s=Number(document.getElementById("display").value);
+    
+    
      }
-if(a!=''&&s!=0){
+    if(a!=''&&s!=0){
 	
 	if(a=='+'){
 		store+=s;
 	}
 	else if(a=='-'){
-		document.getElementById("minus").style.background="green";
 		store-=s;
+		
 	}
 	else if(a=='*'){
 		store*=s;
+		
 	}
 	else if(a=='/'){
 		store/=s;
 	}
+	a='';
 }
-
-	document.getElementById("display").value=" ";
-	
-	
-	
-     
-	if(no==='+'&&store!=0){
+  
+   if(flag==false){
+	temp2='';
+}
+else{
+  temp2='a';
+  }
+	if(no==='+'&&temp2=='a'){
 		
+		flag2=false;	
 		a=no;
 		
-		k='a';
+		
 	document.getElementById("plus").style.background="green";
 	document.getElementById("minus").style.background="";
 	document.getElementById("mul").style.background="";
 	document.getElementById("div").style.background="";
+	
 	}
-	if(no==='-'){
+	if(no==='-'&&temp2=='a'){
 		
 	
-	if(k=='a'&&s==0){
+	if((flag2==false)){
 			
-			document.getElementById("display").value+=no;
+			document.getElementById("display").value=no;
 			document.getElementById("minus").style.background="";
+			temp2='';
 			flag=false;
 		}
 		else if(a!=''&&s==0){
-			document.getElementById("display").value+=no;
+			document.getElementById("display").value=no;
+			temp2='';
 			flag=false;
 		}
 		else if(store==0&&s==0){
-			document.getElementById("display").value+=no;
+			document.getElementById("display").value=no;
+			temp2='';
 			flag=false;
 		}
 		else{
+			temp2='a';
+			flag2=false;
 			document.getElementById("minus").style.background="green";
 	document.getElementById("plus").style.background="";
 	document.getElementById("mul").style.background="";
 	document.getElementById("div").style.background="";
 			a=no;
 			
+			
 		}
 	}
-	if(no==='*'&&store!=0){
+	if(no==='*'&&temp2!=''){
 		
 		a=no;
+	flag2=false;
 	
-		k='a';
 	document.getElementById("mul").style.background="green";
 	document.getElementById("plus").style.background="";
 	document.getElementById("minus").style.background="";
 	document.getElementById("div").style.background="";
-	}
-	if(no==='/'&&store!=0){
 	
-			a=no;
+	}
+	if(no==='/'&&temp2!=''){
+	a=no;
 		
-		k='a';
-		
+		flag2=false;
+	
 	document.getElementById("div").style.background="green";
 	document.getElementById("plus").style.background="";
 	document.getElementById("minus").style.background="";
 	document.getElementById("mul").style.background="";
 	}
+	
+
+}
+function display(no){
+
+	if(temp!=0){
+		document.getElementById("dis").value=temp;
+	
+	}
+		document.getElementById("dis").value+=no;
+		temp=0;
+	
 }
