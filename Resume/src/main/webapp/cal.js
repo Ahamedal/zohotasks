1170f7432bda83f2457048b337e9255298a81251
Ahamedal <94349140+Ahamedal@users.noreply.github.com>
2022-03-21 21:30:51 +0530

    Add files via upload

--- Resume/src/main/webapp/cal.js
+++ Resume/src/main/webapp/cal.js
@@ -5,26 +5,31 @@
  var temp=0;
  var x=0;
  var a='';
- var k='';
+ 
  var s=0;
  var flag=true;	
  var flag2=true;
+ var flag3=true;
  var temp2='';
  function number(no){
-	if(temp!=0||temp2!=''){
+	if(temp2!=''){
 		document.getElementById("display").value="";
 	}
 	
 	document.getElementById("display").value+=no;
+	var commaReplaced=document.getElementById("display").value.replaceAll(",","");
+	document.getElementById("display").value=comma(commaReplaced);
+	
 	flag2=true;
 	temp2='';
 	flag=true;
    
-     
+   
  }
 function result(r){
 	var y=0;
 	flag=true;
+	flag3=true;
 	document.getElementById("plus").style.background="";
 	document.getElementById("minus").style.background="";
 	document.getElementById("mul").style.background="";
@@ -35,16 +40,16 @@ function result(r){
 		store=0;
          s=0;
          a='';
-         k='';
+    
           temp2='';
            temp=0;
     
 	}
 	
 	else{
-	
+	document.getElementById("display").value=document.getElementById("display").value.replaceAll(",","");
    x=Number(document.getElementById("display").value);
- 
+  
   if(a=='+'){
   y=store+x;
   }
@@ -62,29 +67,32 @@ function result(r){
   else{
 	y=x;
 }
-  document.getElementById("display").value=y;
+  document.getElementById("display").value=comma(y);
   temp=y;
  temp2='';
   store=0;
   s=0;
   a='';
-  k='';
+ 
   }
 
 }
 function operator(no){
 	
 	
-	if(store==0){
-	store=Number(document.getElementById("display").value);
+	if(store==0&&flag3){
+	var aa=document.getElementById("display").value.replaceAll(",","");
+	
+	store=Number(aa);
+	flag3=false;
 	}
 	else if(flag2){
 		 
-    s=Number(document.getElementById("display").value);
+    s=Number(document.getElementById("display").value.replaceAll(",",""));
     
     
      }
-    if(a!=''&&s!=0){
+    if(a!=''&&s!=0&&flag2){
 	
 	if(a=='+'){
 		store+=s;
@@ -178,12 +186,76 @@ else{
 
 }
 function display(no){
+	if(no=='-'&&document.getElementById("dis").value==""){
+		flag3=false;
+	}
+	if(document.getElementById("dis").value=="-"){
+	flag3=true;
+	}
+	if((no=='+'||no=='*'||no=='/')&&document.getElementById("dis").value==""){
+document.getElementById("display").value=0;
+document.getElementById("dis").value=0;
+store=0;
+
+}
 
 	if(temp!=0){
 		document.getElementById("dis").value=temp;
 	
 	}
-		document.getElementById("dis").value+=no;
+	   
+	    
+		var ans=document.getElementById("dis").value+=no;
+		
+	 if(!flag2&&no!='-'){
+		if(ans.charAt(ans.length-2)=='-'){
+			var sub=ans.substring(0,ans.length-2);
+			document.getElementById("dis").value=sub+'-';
+			
+		}
+		else{
+		var sub=ans.substring(0,ans.length-2);
+		document.getElementById("dis").value=sub+no;
+		}
+	}
+		
 		temp=0;
+	
+}
+function comma(value){
+	var str=value.toString();
+	
+	var sp=str.split(".");
+	
+	var res=[];
+	
+
+	
+	var a = 0, b = 0;
+    for (var i = sp[0].length- 1; i >= 0; i--) {
+        if (a < 3) {
+            res.push(sp[0][i]);
+            a++;
+        } else if (b < 2) {
+            if (b == 0&&sp[0][i]!='-') {
+                res.push(",");
+                res.push(sp[0][i]);
+                b++;
+            } else {
+                res.push(sp[0][i]);
+                b = 0;
+            }
+        }
+    }
+    var k=res.reverse();
+    k=k.join(""); 
+    if(sp[1]==undefined){
+    k=k;
+    }
+    else{
+	k=k+"."+sp[1];
+}
+    return k;
+	
 	
 }
